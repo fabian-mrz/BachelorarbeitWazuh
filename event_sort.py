1c9896c1118d8316618015b3a61d10587c0fe8b7
Fabian Merz <76883238+FabianMerz@users.noreply.github.com>
2024-10-06 00:55:38 +0200

    Added basic suppression functionality

--- event_sort.py
+++ event_sort.py
@@ -1,27 +1,42 @@
-#!/usr/bin/env python
-
-import sys
-import json
-import configparser
 import os
+import json
+import sys
+from datetime import datetime
 from threading import Lock
 
-# Read configuration from config.ini
-config = configparser.ConfigParser()
-config.read('/var/ossec/integrations/config.ini')
-
-# Read the alert file
-alert_file = open(sys.argv[1])
-alert_json = json.loads(alert_file.read())
-alert_file.close()
-
-# Extract rule_id
-rule_id = alert_json['rule']['id'] if 'id' in alert_json['rule'] else "N/A"
-
-# Define a lock for thread-safe file operations
 lock = Lock()
 
+def parse_date(date_str):
+    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+def evaluate_expression(expression, alert_json):
+    return eval(expression)
+
+def is_suppressed(alert_json, suppression_criteria):
+    for criteria in suppression_criteria:
+        match = True
+        suppression_fields = criteria["fields"]
+        for key, value in criteria.items():
+            if key == "timeframe":
+                start_time, end_time = value.split('/')
+                event_time = parse_date(evaluate_expression(suppression_fields["timestamp"], alert_json))
+                if not (parse_date(start_time) <= event_time <= parse_date(end_time)):
+                    match = False
+                    break
+            elif key != "fields":
+                field_value = evaluate_expression(suppression_fields[key], alert_json)
+                if field_value != value:
+                    match = False
+                    break
+        if match:
+            return True
+    return False
+
 def save_event(rule_id, alert_json):
+    if is_suppressed(alert_json, suppression_criteria):
+        print(f"Event with rule_id {rule_id} is suppressed.")
+        return
+
     # Create the events directory if it doesn't exist
     events_dir = '/var/ossec/integrations/events'
     if not os.path.exists(events_dir):
@@ -39,5 +54,15 @@ def save_event(rule_id, alert_json):
             with open(file_path, 'w') as file:
                 json.dump([alert_json], file)
 
+# Load suppression criteria from suppression.json
+with open('/var/ossec/integrations/suppression.json') as suppression_file:
+    suppression_data = json.load(suppression_file)
+    suppression_criteria = suppression_data['criteria']
+
+# Read the alert file
+alert_file = open(sys.argv[1])
+alert_json = json.loads(alert_file.read())
+alert_file.close()
+
 # Save the event
-save_event(rule_id, alert_json)
+save_event(alert_json["rule"]["id"], alert_json)
